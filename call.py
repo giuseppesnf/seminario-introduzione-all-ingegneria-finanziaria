e9281ed55ef23c7c1e617fbdeb19afbb1d73f804
giuseppesnf <64224435+giuseppesnf@users.noreply.github.com>
2020-05-28 12:59:56 +0200

    Add files via upload

---
+++ call.py
@@ -0,0 +1,57 @@
+from QuantLib import *
+import matplotlib.pyplot as plot
+import numpy as np
+
+today = Date(27, September, 2019)
+Settings.instance().evaluationDate = today
+option = EuropeanOption(PlainVanillaPayoff(Option.Call, 38129.0), EuropeanExercise(Date(27, September, 2024)))
+
+u = SimpleQuote(259594.0)
+r = SimpleQuote(0.0169)
+sigma = SimpleQuote(0.2)
+
+riskFreeCurve = FlatForward(0, TARGET(), QuoteHandle(r), Actual360())
+volatility = BlackConstantVol(0, TARGET(), QuoteHandle(sigma), Actual360())
+
+process = BlackScholesProcess(QuoteHandle(u), YieldTermStructureHandle(riskFreeCurve), BlackVolTermStructureHandle(volatility))
+
+engine = AnalyticEuropeanEngine(process)
+
+option.setPricingEngine(engine)
+
+print(option.NPV())
+print(option.delta())
+print(option.gamma())
+print(option.vega())
+
+
+
+f, ax = plot.subplots()
+xs = np.linspace(100.00, 40000.00)
+ys = []
+
+
+
+
+for x in xs:
+    u.setValue(x)
+    ys.append(option.NPV())
+
+ax.set_title("Option Value")
+_ = ax.plot(xs, ys)
+plot.show()
+
+model = HestonModel(
+    HestonProcess(YieldTermStructureHandle(riskFreeCurve),
+                  YieldTermStructureHandle(FlatForward(0, TARGET(), 0.0, Actual360())),
+                  QuoteHandle(u),
+                  0.004, 0.1, 0.01, 0.05, -0.75))
+engine = AnalyticHestonEngine(model)
+option.setPricingEngine(engine)
+print(option.NPV())
+
+engine = MCEuropeanEngine(process, "PseudoRandom",
+                          timeSteps=20,
+                          requiredSamples=250000)
+option.setPricingEngine(engine)
+print(option.NPV())
